9e0cc895b43f868dc0697c45d4e613c9fcd2caaa
Hien Nguyen <hien.nguyen@mingle.com>
2023-03-22 10:27:05 +0700

    allow custom authorization, token url

--- lib/model/config.dart
+++ lib/model/config.dart
@@ -15,18 +15,23 @@ class Config {
   final String nonce;
 
   ///ResponseType to mobile usually is "code", and web usually is "id_token+token"
-  Config(
-      {required this.azureTenantId,
-      required this.clientId,
-      required this.scope,
-      required this.redirectUri,
-      required this.responseType,
-      this.clientSecret,
-      this.resource,
-      this.contentType = 'application/x-www-form-urlencoded',
-      this.context,
-      this.nonce = 'nonce_value'}) {
-    authorizationUrl = 'https://login.microsoftonline.com/$azureTenantId/oauth2/v2.0/authorize';
-    tokenUrl = 'https://login.microsoftonline.com/$azureTenantId/oauth2/v2.0/token';
+  Config({
+    required this.azureTenantId,
+    required this.clientId,
+    required this.scope,
+    required this.redirectUri,
+    required this.responseType,
+    this.clientSecret,
+    this.resource,
+    this.contentType = 'application/x-www-form-urlencoded',
+    this.context,
+    this.nonce = 'nonce_value',
+    this.authorizationUrl,
+    this.tokenUrl,
+  }) {
+    authorizationUrl = authorizationUrl ??
+        'https://login.microsoftonline.com/$azureTenantId/oauth2/v2.0/authorize';
+    tokenUrl = tokenUrl ??
+        'https://login.microsoftonline.com/$azureTenantId/oauth2/v2.0/token';
   }
 }
